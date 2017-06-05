e409a1eca51fb913492431bc0475c2733dce37a8
sorrowbeaver <jkj8790@gmail.com>
2017-06-05 16:17:53 +0900

    Add leakcanary initialization

---
+++ app/src/main/java/com/sorrowbeaver/momo/MomoApplication.kt
@@ -0,0 +1,15 @@
+package com.sorrowbeaver.momo
+
+import android.app.Application
+import com.squareup.leakcanary.LeakCanary
+
+class MomoApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            return
+        }
+        LeakCanary.install(this)
+    }
+}
+
