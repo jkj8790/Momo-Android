f582156bcf0906a03f19586edb36cb4c30eb091e
sorrowbeaver <jkj8790@gmail.com>
2017-06-06 23:49:42 +0900

    Complement UserRepository

--- domain/src/main/java/com/sorrowbeaver/momo/domain/repository/UserRepository.kt
+++ domain/src/main/java/com/sorrowbeaver/momo/domain/repository/UserRepository.kt
@@ -2,10 +2,25 @@ package com.sorrowbeaver.momo.domain.repository
 
 import com.sorrowbeaver.momo.domain.model.User
 import io.reactivex.Observable
+import okhttp3.Response
 
 interface UserRepository {
 
-  fun login() : Observable<User>
+  fun login(userName: String, password: String) : Observable<User>
 
+  fun singup(email: String, userName: String, password: String) : Observable<User>
+
+  //TODO ordering
+  fun users() : Observable<Response>
+
+  fun logout() : Observable<Response>
+
+  fun facebookLogin() : Observable<User>
+
+  fun detail(userId: Long) : Observable<User>
+
+  fun follow(userId : Long) : Observable<Response>
+
+  fun requestAuthentiacateMail() : Observable<Response>
 }
 
