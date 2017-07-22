f03e9ffd3cf2b9cb4b84b5e6612598981d622520
sorrowbeaver <jkj8790@gmail.com>
2017-07-22 18:55:08 +0900

    Change UserRepository specification

---
+++ domain/src/main/java/com/sorrowbeaver/momo/domain/model/UserSortOption.kt
@@ -0,0 +1,5 @@
+package com.sorrowbeaver.momo.domain.model
+
+enum class UserSortOption {
+  FOLLOWER_DESCENDING, MAP_DESCENDING
+}
--- domain/src/main/java/com/sorrowbeaver/momo/domain/repository/UserRepository.kt
+++ domain/src/main/java/com/sorrowbeaver/momo/domain/repository/UserRepository.kt
@@ -1,26 +1,29 @@
 package com.sorrowbeaver.momo.domain.repository
 
 import com.sorrowbeaver.momo.domain.model.User
+import com.sorrowbeaver.momo.domain.model.UserSortOption
 import io.reactivex.Observable
 import okhttp3.Response
 
 interface UserRepository {
+  //TODO Response depends on HTTP specification. Better if implement own response object
+  //TODO And consider own exception object too.
 
   fun login(id: String, password: String): Observable<User>
 
   fun signUp(email: String, userName: String, password: String): Observable<User>
 
-  //TODO ordering
-  fun users(): Observable<Response>
+  fun facebookLogin(): Observable<User>
+
+  fun requestAuthenticateMail(): Observable<Response>
 
   fun logout(): Observable<Response>
 
-  fun facebookLogin(): Observable<User>
-
   fun detail(userId: Long): Observable<User>
 
-  fun follow(userId: Long): Observable<Response>
+  //TODO Add sort option parameter.
+  fun users(sortOption: UserSortOption): Observable<List<User>>
 
-  fun requestAuthentiacateMail(): Observable<Response>
+  fun follow(userId: Long): Observable<Response>
 }
 
