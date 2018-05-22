42c625f6a786ac7d761d0dd9ba44c9d9d21bb7b8
sorrowbeaver <jkj8790@gmail.com>
2018-05-22 17:22:26 +0900

    Add inject annotated constructor

--- presentation/src/main/kotlin/com/sorrowbeaver/momo/mapper/LocationModelDataMapper.kt
+++ presentation/src/main/kotlin/com/sorrowbeaver/momo/mapper/LocationModelDataMapper.kt
@@ -2,9 +2,10 @@ package com.sorrowbeaver.momo.mapper
 
 import com.sorrowbeaver.momo.domain.model.Location
 import com.sorrowbeaver.momo.model.LocationModel
+import javax.inject.Inject
 
-class LocationModelDataMapper {
-  fun transform(location: Location) = LocationModel(
+open class LocationModelDataMapper @Inject constructor() {
+  open fun transform(location: Location) = LocationModel(
     location.latitude,
     location.longitude
   )
